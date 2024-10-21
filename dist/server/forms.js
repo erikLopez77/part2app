db4637c25404db456469869659cb6e61d2acf615
ErikLopez <espinozalopezerik@gmail.com>
2024-10-20 18:32:47 -0600

    nueva plantilla age y validacion

--- dist/server/forms.js
+++ dist/server/forms.js
@@ -5,8 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerFormRoutes = exports.registerFormMiddleware = void 0;
 const express_1 = __importDefault(require("express"));
-const multer_1 = __importDefault(require("multer"));
-const fileMiddleware = (0, multer_1.default)({ storage: multer_1.default.memoryStorage() });
+//import multer from "multer";
+//import { sanitizeValue } from "./sanitize";
+//const fileMiddleware = multer({ storage: multer.memoryStorage() });
 const registerFormMiddleware = (app) => {
     //extended se permiten datos mas complejos a ser procesados, se le da formato
     app.use(express_1.default.urlencoded({ extended: true }));
@@ -14,16 +15,13 @@ const registerFormMiddleware = (app) => {
 exports.registerFormMiddleware = registerFormMiddleware;
 const registerFormRoutes = (app) => {
     app.get("/form", (req, resp) => {
-        for (const key in req.query) {
-            resp.write(`${key}: ${req.query[key]}\n`);
-        }
-        resp.end();
+        resp.render("age");
     });
-    app.post("/form", fileMiddleware.single("datafile"), (req, resp) => {
+    app.post("/form", (req, resp) => {
         //renderiza a plantilla formData
-        resp.render("formData", {
-            ...req.body, file: req.file,
-            fileData: req.file?.buffer.toString()
+        resp.render("age", {
+            ...req.body,
+            nextage: Number.parseInt(req.body.age) + 1
         });
     });
 };
--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,26 +1,22 @@
 import express, { Express } from "express";
-import multer from "multer";
-import { sanitizeValue } from "./sanitize";
+//import multer from "multer";
+//import { sanitizeValue } from "./sanitize";
 
-const fileMiddleware = multer({ storage: multer.memoryStorage() });
+//const fileMiddleware = multer({ storage: multer.memoryStorage() });
 export const registerFormMiddleware = (app: Express) => {
     //extended se permiten datos mas complejos a ser procesados, se le da formato
     app.use(express.urlencoded({ extended: true }))
 }
 export const registerFormRoutes = (app: Express) => {
     app.get("/form", (req, resp) => {
-
-        for (const key in req.query) {
-            resp.write(`${key}: ${req.query[key]}\n`);
-        }
-        resp.end();
+        resp.render("age");
     });
 
-    app.post("/form", fileMiddleware.single("datafile"), (req, resp) => {
+    app.post("/form", (req, resp) => {
         //renderiza a plantilla formData
-        resp.render("formData", {
-            ...req.body, file: req.file,
-            fileData: req.file?.buffer.toString()
+        resp.render("age", {
+            ...req.body,
+            nextage: Number.parseInt(req.body.age) + 1
         });
     });
 }
